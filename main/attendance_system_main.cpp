9fc06e72913895b4b9fc49a4a0ed90e7813944f1
AlphaGoosling <henly9@protonmail.com>
2025-03-11 16:47:27 +0300

    created separate cpp file for function definitions

--- main/attendance_system_main.cpp
+++ main/attendance_system_main.cpp
@@ -1,23 +1,4 @@
-#include "Arduino.h"
-#include "Adafruit_Fingerprint.h"
-#include "TFT_eSPI.h"
-#include "FS.h"
-
-extern "C"{
-    #include <string.h>
-    #include <stdio.h>
-    #include "freertos/FreeRTOS.h"
-    #include "freertos/task.h"
-    #include "freertos/event_groups.h"
-    #include "esp_system.h"
-    #include "esp_wifi.h"
-    #include "esp_event.h"
-    #include "esp_log.h"
-    #include "nvs_flash.h"
-    #include "lwip/err.h"
-    #include "lwip/sys.h"  
-    #include <golioth/client.h>  
-}
+#include "utils.h"
 
 TaskHandle_t FingerprintScannerTaskHandler = NULL;
 TaskHandle_t DisplayTaskHandler = NULL;
@@ -26,6 +7,14 @@ TaskHandle_t GoliothTaskHandler = NULL;
 void Fingerprint_Scanner_Task(void *arg);
 void Display_Task(void *arg);
 void Golioth_Task(void *arg);
+
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+HardwareSerial fingerprintSerial(2);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerprintSerial);
+
 
 #define CALIBRATION_FILE "/calibrationData"
  
@@ -78,97 +67,6 @@ static const char *TAG = "wifi station mode";
 
 static int s_retry_num = 0;
 
-static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG,"connect to the AP fail");
-
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
-
-extern "C" void wifi_init_sta(void)
-{
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = esp_wifi_ssid,
-            .password = esp_wifi_password,
-            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (password len => 8).
-             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
-             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
-             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
-             */
-            .threshold = {.rssi = -90, .authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD, .rssi_5g_adjustment = 20},
-            .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
-            .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 esp_wifi_ssid, esp_wifi_password);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 esp_wifi_ssid, esp_wifi_password);
-    } else {
-        ESP_LOGI(TAG, "UNEXPECTED EVENT");
-    }
-}
-
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
 
 extern "C" void app_main(void)
@@ -188,93 +86,6 @@ extern "C" void app_main(void)
     xTaskCreate(Fingerprint_Scanner_Task, "Fingerprint Scanner", 4096, NULL, 3, &FingerprintScannerTaskHandler);
     xTaskCreate(Display_Task, "Display", 4096, NULL, 3, &DisplayTaskHandler);
     xTaskCreate(Golioth_Task, "Golioth", 4096, NULL, 3, &GoliothTaskHandler);
-}
-
-HardwareSerial fingerprintSerial(2);
-
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerprintSerial);
-
-uint8_t getFingerprintID() {
-  uint8_t p = finger.getImage();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println("No finger detected");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK success!
-
-  p = finger.image2Tz();
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK converted!
-  p = finger.fingerSearch();
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Found a print match!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_NOTFOUND) {
-    Serial.println("Did not find a match");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-
-  // found a match!
-  Serial.print("Found ID #"); Serial.print(finger.fingerID);
-  Serial.print(" with confidence of "); Serial.println(finger.confidence);
-
-  return finger.fingerID;
-}
-
-// returns -1 if failed, otherwise returns ID #
-int getFingerprintIDez() {
-  uint8_t p = finger.getImage();
-  if (p != FINGERPRINT_OK)  return -1;
-
-  p = finger.image2Tz();
-  if (p != FINGERPRINT_OK)  return -1;
-
-  p = finger.fingerFastSearch();
-  if (p != FINGERPRINT_OK)  return -1;
-
-  // found a match!
-  Serial.print("Found ID #"); Serial.print(finger.fingerID);
-  Serial.print(" with confidence of "); Serial.println(finger.confidence);
-  return finger.fingerID;
 }
 
 void Fingerprint_Scanner_Task(void *arg)
@@ -316,6 +127,7 @@ void Fingerprint_Scanner_Task(void *arg)
     vTaskDelay(50 / portTICK_PERIOD_MS);
   }
 }
+
 void Display_Task(void *arg)
 {
   uint16_t calibrationData[5];
---
+++ main/utils.h
@@ -0,0 +1,40 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+ 
+#include "Arduino.h"
+#include "Adafruit_Fingerprint.h"
+#include "TFT_eSPI.h"
+#include "FS.h"
+
+extern "C"{
+    #include <string.h>
+    #include <stdio.h>
+    #include "freertos/FreeRTOS.h"
+    #include "freertos/task.h"
+    #include "freertos/event_groups.h"
+    #include "esp_system.h"
+    #include "esp_wifi.h"
+    #include "esp_event.h"
+    #include "esp_log.h"
+    #include "nvs_flash.h"
+    #include "lwip/err.h"
+    #include "lwip/sys.h"  
+    #include <golioth/client.h>  
+}
+
+#define CALIBRATION_FILE "/calibrationData"
+ 
+#define esp_wifi_ssid      "redmi-black"
+#define esp_wifi_password      "77777777"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  0
+
+extern const char* psk_id;
+extern const char* psk;
+
+extern "C" void wifi_init_sta(void);
+
+uint8_t getFingerprintID();
+int getFingerprintIDez();
+
+
+#endif 
